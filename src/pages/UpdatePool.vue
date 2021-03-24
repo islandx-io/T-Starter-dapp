ba1948aa8981cd3ee038281eb8cda7fe2d610b87
Dewald928 <dewald123@rocketmail.com>
2021-03-24 14:19:41 +0200

    whitelist placeholder

--- src/pages/UpdatePool.vue
+++ src/pages/UpdatePool.vue
@@ -278,6 +278,7 @@
                     outlined
                     :rules="[whitelistValidate]"
                     debounce="500"
+                    placeholder="Leave empty if no whitelist needed"
                   /> </q-item-section
               ></q-item>
             </q-list>
