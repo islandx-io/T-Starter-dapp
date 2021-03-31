307f019f2c8feaed64c52733e7f53de0040d3c02
JPBeukes <jpbeukes27@gmail.com>
2021-03-31 15:32:33 +0200

    Login: Added create-account-carousel

--- src/components/LoginButton.vue
+++ src/components/LoginButton.vue
@@ -43,67 +43,107 @@
       </q-btn>
     </q-btn-group>
     <q-dialog v-model="showLogin">
-      <div
-        class="column items-center q-gutter-y-sm q-pa-md"
-        style="box-shadow: none"
-      >
-        <q-list class="shadow-5">
-          <q-item
-            v-for="(wallet, idx) in $ual.authenticators"
-            :key="wallet.getStyle().text"
-            v-ripple
-            :style="{
-              background: wallet.getStyle().background,
-              color: wallet.getStyle().textColor
-            }"
+      <div style="box-shadow: none">
+        <q-carousel
+          v-model="dialogSlide"
+          transition-prev="jump-down"
+          transition-next="jump-up"
+          animated
+          control-color="primary"
+          class="column flex-center"
+          style="background: transparent; overflow-x: hidden;"
+        >
+          <q-carousel-slide
+            name="loginSlide"
+            class="column flex-center q-gutter-y-sm q-pa-md"
           >
-            <q-item-section class="cursor-pointer" avatar @click="onLogin(idx)">
-              <img :src="wallet.getStyle().icon" width="30" />
-            </q-item-section>
-            <q-item-section class="cursor-pointer" @click="onLogin(idx)">
-              {{ wallet.getStyle().text }}
-            </q-item-section>
-            <q-item-section class="flex" avatar>
-              <q-spinner
-                v-if="loading === wallet.getStyle().text"
-                :color="wallet.getStyle().textColor"
-                size="2em"
-              />
-              <q-btn
-                v-else
-                :color="wallet.getStyle().textColor"
-                icon="get_app"
-                @click="openUrl(wallet.getOnboardingLink())"
-                target="_blank"
-                dense
-                flat
-                size="12px"
+            <q-list class="shadow-5">
+              <q-item
+                v-for="(wallet, idx) in $ual.authenticators"
+                :key="wallet.getStyle().text"
+                v-ripple
+                :style="{
+                  background: wallet.getStyle().background,
+                  color: wallet.getStyle().textColor
+                }"
               >
-                <q-tooltip>
-                  Get app
-                </q-tooltip>
-              </q-btn>
-            </q-item-section>
-          </q-item>
-          <q-item
-            v-if="error"
-            :active="!!error"
-            active-class="bg-red-1 text-grey-8"
-          >
-            <q-item-section>
-              {{ error }}
-            </q-item-section>
-          </q-item>
-        </q-list>
-        <q-btn
-          label="Create New Account"
-          class="hover-accent text-secondary"
-          outline
-          flat
-          type="a"
-          href="https://telos.net/create-account/"
-          target="_blank"
-        />
+                <q-item-section
+                  class="cursor-pointer"
+                  avatar
+                  @click="onLogin(idx)"
+                >
+                  <img :src="wallet.getStyle().icon" width="30" />
+                </q-item-section>
+                <q-item-section class="cursor-pointer" @click="onLogin(idx)">
+                  {{ wallet.getStyle().text }}
+                </q-item-section>
+                <q-item-section class="flex" avatar>
+                  <q-spinner
+                    v-if="loading === wallet.getStyle().text"
+                    :color="wallet.getStyle().textColor"
+                    size="2em"
+                  />
+                  <q-btn
+                    v-else
+                    :color="wallet.getStyle().textColor"
+                    icon="get_app"
+                    @click="openUrl(wallet.getOnboardingLink())"
+                    target="_blank"
+                    dense
+                    flat
+                    size="12px"
+                  >
+                    <q-tooltip>
+                      Get app
+                    </q-tooltip>
+                  </q-btn>
+                </q-item-section>
+              </q-item>
+              <q-item
+                v-if="error"
+                :active="!!error"
+                active-class="bg-red-1 text-grey-8"
+              >
+                <q-item-section>
+                  {{ error }}
+                </q-item-section>
+              </q-item>
+            </q-list>
+            <q-btn
+              label="Create New Account"
+              class="hover-accent text-secondary"
+              outline
+              flat
+              @click="dialogSlide = 'createSlide'"
+            />
+          </q-carousel-slide>
+          <q-carousel-slide class="column flex-center" name="createSlide">
+            <q-card>
+              <p>
+                Create a Telos account through the official telos.net portal and
+                then login with the wallet of your choice. We recommend using
+                Anchor.
+              </p>
+              <div class="row justify-between">
+                <q-btn
+                  label="Create account"
+                  class="hover-accent text-accent"
+                  outline
+                  type="a"
+                  href="https://telos.net/create-account/"
+                  target="_blank"
+                />
+                <q-btn
+                  label="Back"
+                  class="text-black"
+                  flat
+                  outline
+                  @click="dialogSlide = 'loginSlide'"
+                />
+              </div>
+            </q-card>
+          </q-carousel-slide>
+        </q-carousel>
       </div>
     </q-dialog>
   </div>
@@ -119,7 +159,8 @@ export default {
       showLogin: false,
       error: null,
       showLogout: false,
-      balanceSTR: "0 START"
+      balanceSTR: "0 START",
+      dialogSlide: "loginSlide"
     };
   },
   props: {
@@ -189,15 +230,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-// .account-badge {
-//   border-radius: 16px;
-//   max-height: 32px;
-//   padding: 4px 10px;
-//   background-color: $primary;
-//   font-size: 14px;
-//   font-weight: bold;
-//   color: $secondary;
-// }
 .login {
   padding-right: 0;
 }
@@ -211,5 +243,9 @@ export default {
   font-size: 14px;
   font-weight: bold;
   color: $secondary;
+}
+.q-carousel__slide {
+  overflow-x: hidden;
+  width: 300px;
 }
 </style>
