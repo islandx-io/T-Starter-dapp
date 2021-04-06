ca9c20e3f704bd900b468c75b0b8c379fcdd9e9b
JPBeukes <jpbeukes27@gmail.com>
2021-04-06 06:46:21 +0200

    ReceiveTokens page: Improved layout

--- src/pages/ReceiveTokens.vue
+++ src/pages/ReceiveTokens.vue
@@ -1,13 +1,58 @@
 <template>
-  <q-page>
-    <section>
-      <div>I am receiving {{ $route.query }}</div>
-      <div v-if="devWarning">Don't do real payments</div>
-      <div>{{ BTCaddress }}</div>
-      <img :src="QRcode" />
-
-      <div id="canvas"></div>
-    </section>
+  <q-page class="column">
+    <section class="header-bg" />
+    <div class="self-center flex-shrink">
+      <q-card class="card-container">
+        <q-btn :to="`/wallet/${accountName}`" flat round class="self-start">
+          <q-icon name="fas fa-chevron-circle-left" style="font-size: 50px;" />
+        </q-btn>
+        <div>
+          <div class="column items-center">
+            <h2 style="line-height: 45px; text-align: center">
+              Receive {{ $route.query.token_sym }}
+            </h2>
+            <div class="text-subtitle1 q-pb-sm">From network</div>
+            <div class="networks row q-gutter-x-md">
+              <q-btn
+                label="Telos"
+                @click="network = 'telos'"
+                :class="network === 'telos' ? 'selected-network' : ''"
+                flat
+                size="lg"
+                no-caps
+                padding="xs"
+              />
+              <q-btn
+                label="Bitcoin"
+                @click="network = 'bitcoin'"
+                :class="network === 'bitcoin' ? 'selected-network' : ''"
+                flat
+                size="lg"
+                no-caps
+                padding="xs"
+              />
+            </div>
+            <div class="text-subtitle1 q-py-md">
+              Deposit {{ currency }} to the following address
+            </div>
+            <div v-if="devWarning">Don't do real payments</div>
+            <!-- <img :src="QRcode" /> -->
+            <div id="canvas" />
+            <div class="text-subtitle1">
+              {{ BTCaddress }}
+              <q-btn
+                flat
+                @click="copyAddress(BTCaddress)"
+                icon="far fa-clone"
+                padding="0"
+                color="positive"
+                size="sm"
+              />
+            </div>
+          </div>
+        </div>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
@@ -16,13 +61,13 @@ import axios from "axios";
 import { mapGetters, mapActions } from "vuex";
 import QRCodeStyling from "qr-code-styling";
 import QRCode from "qrcode";
+import { copyToClipboard } from "quasar";
 
 const QR = new QRCodeStyling({
-  width: 300,
-  height: 300,
+  width: 250,
+  height: 250,
   data: "",
-  image:
-    "/tokens/bitcoin.svg",
+  image: "/tokens/bitcoin.svg",
   dotsOptions: {
     type: "dots",
     color: "#fdb435",
@@ -42,7 +87,7 @@ const QR = new QRCodeStyling({
     }
   },
   backgroundOptions: {
-    color: "#e9ebee"
+    color: "#f9fbfe"
   },
   imageOptions: {
     crossOrigin: "anonymous",
@@ -56,13 +101,25 @@ export default {
       devWarning: process.env.DEVELOPMENT,
       BTCaddress: "",
       QRcode: "",
-      qrCode: QR
+      qrCode: QR,
+      network: "telos",
+      currency: "pBTC"
     };
   },
   computed: {
     ...mapGetters("account", ["isAuthenticated", "accountName", "wallet"])
   },
   methods: {
+    copyAddress(adress) {
+      copyToClipboard(adress).then(() => {
+        this.$q.notify({
+          color: "green-4",
+          textColor: "secondary",
+          message: "Copied account name to clipboard",
+          timeout: 1000
+        });
+      });
+    },
     async generateQR(text) {
       try {
         return await QRCode.toDataURL(text);
@@ -78,13 +135,36 @@ export default {
       this.BTCaddress = pbridge_api.data.nativeDepositAddress || [];
 
       this.QRcode = await this.generateQR(this.BTCaddress);
-      this.qrCode.update({data: this.BTCaddress})
+      this.qrCode.update({ data: this.BTCaddress });
     }
   },
   mounted() {
     this.setBTCaddress();
     this.qrCode.append(document.getElementById("canvas"));
-
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.card-container {
+  display: grid;
+  align-items: stretch;
+  grid-template-columns: 50px auto 50px;
+}
+.header-bg {
+  height: 160px;
+  margin-bottom: -50px;
+}
+
+.networks {
+  .q-btn {
+    background-color: silver;
+    color: $secondary;
+    width: 120px;
+    align-items: center;
+  }
+  .q-btn.selected-network {
+    background-color: $space;
+  }
+}
+</style>
