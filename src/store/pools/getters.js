64503e2e7b5e79df4d8167dd32f34998fb89f638
Dewald928 <dewald123@rocketmail.com>
2021-04-13 10:41:18 +0200

    sort date fix

--- src/store/pools/getters.js
+++ src/store/pools/getters.js
@@ -5,11 +5,8 @@ export const getAllPoolIDs = ({ pools }) => pools.map(a => a.id);
 // sorts closest to furthest openening date
 export const getPoolIDsByStatus = ({ pools }) => pool_status =>
   pools.filter(a => a.pool_status === pool_status).sort((a, b) => {
-    return new Date(b.pool_open) - new Date(a.pool_open);
+    return new Date(a.pool_open) - new Date(b.pool_open);
   }).map(a => a.id);
-
-export const getUpcomingPools = ({ pools }) =>
-  pools.filter(a => a.pool_status === "upcoming");
 
 export const getPoolByID = ({ pools }) => id => pools.find(el => el.id === id);
 
