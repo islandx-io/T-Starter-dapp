f4a32590f6826b6550fcf7908ef650dc474f458d
Dewald928 <dewald123@rocketmail.com>
2021-04-16 12:11:38 +0200

    ios anchor enable

--- src/components/LoginButton.vue
+++ src/components/LoginButton.vue
@@ -70,7 +70,7 @@
                   background: wallet.getStyle().background,
                   color: wallet.getStyle().textColor
                 }"
-                v-show="wallet.getStyle().text === 'Telos Sign' && isMobile || !isMobile"
+                v-show="wallet.getStyle().text === 'Telos Sign' && isMobile || !isMobile || PlatformInfo.is.ios && wallet.getStyle().text === 'Anchor' && isMoblie" 
               >
                 <q-item-section
                   class="cursor-pointer"
