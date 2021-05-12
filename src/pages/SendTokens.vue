e50028b63c969713d4e7fe9076be0ad5a1d9dabd
JPBeukes <jpbeukes27@gmail.com>
2021-05-12 15:32:33 +0200

    Send page: Added cross-chain warning

--- src/pages/SendTokens.vue
+++ src/pages/SendTokens.vue
@@ -155,6 +155,16 @@
                 type="submit"
               />
             </div>
+            <div
+              class="text-center text-caption q-pt-md text-grey-7"
+              v-if="
+                selectedNetwork.toUpperCase() !==
+                  currentChain.NETWORK_NAME.toUpperCase()
+              "
+            >
+              <q-icon name="fas fa-info-circle" /> Sending tokens across chains
+              can take up to several minutes.
+            </div>
 
             <!-- Transaction sent dialog -->
             <q-dialog v-model="showTransaction" confirm>
