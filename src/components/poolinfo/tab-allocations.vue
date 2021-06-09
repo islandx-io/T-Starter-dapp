7ddc6d37661b5b4b2b239a49bbabe215f302cd35
JPBeukes <jpbeukes27@gmail.com>
2021-06-09 10:33:24 +0200

    Allocations: Finished vesting upgrade

--- src/components/poolinfo/tab-allocations.vue
+++ src/components/poolinfo/tab-allocations.vue
@@ -1,43 +1,54 @@
 <template>
   <div class="column justify-between">
-    <div class="row justify-between" v-if="hasAllocations">
+    <div class="row justify-between" v-if="hasAllocation">
+      <h6>Bid:</h6>
+      <h5>{{ this.$chainStrReformat(allocationData.bid) }}</h5>
+    </div>
+    <div class="row justify-between" v-if="hasAllocation">
       <h6>Allocation:</h6>
       <h5>
         {{ this.$chainStrReformat(allocationData.allocation, tokenDecimals) }}
       </h5>
     </div>
-    <div class="row justify-between" v-if="hasAllocations">
+    <div
+      class="row justify-between"
+      v-if="hasAllocation && this.pool.token_lockup"
+    >
       <h6>Vesting:</h6>
       <h5>{{ this.$toChainString(vesting, tokenDecimals, tokenSymbol) }}</h5>
     </div>
-    <div class="row justify-between" v-if="hasAllocations">
+    <div
+      class="row justify-between"
+      v-if="hasAllocation && this.pool.token_lockup"
+    >
       <h6>To be claimed:</h6>
-      <h5>{{ this.$toChainString(claimable, tokenDecimals, tokenSymbol) }}</h5>
-    </div>
-    <div class="row justify-between" v-if="hasAllocations">
-      <h6>Claimed:</h6>
-      <h5>{{ this.$toChainString(claimed, tokenDecimals, tokenSymbol) }}</h5>
+      <h5>{{ this.$toChainString(available, tokenDecimals, tokenSymbol) }}</h5>
     </div>
     <div
-      v-if="hasAllocations"
+      class="row justify-between"
+      v-if="hasAllocation && this.pool.token_lockup"
+    >
+      <h6>Claimed:</h6>
+      <h5>
+        {{ this.$toChainString(distributed, tokenDecimals, tokenSymbol) }}
+      </h5>
+    </div>
+    <div
+      v-if="hasAllocation"
       class="text-left q-pt-md text-caption text-grey-7"
     >
       <q-icon name="fas fa-info-circle" class="q-pr-xs" />Your tokens will be
       sent as soon as the pool is closed. In the event that the number of
       participating accounts become too big for the Telos blockchain to send all
       tokens in a single transaction, you will be able to claim your allocation
-      from this tab.
+      from this tab, 24 hours after the pool ends.
     </div>
     <q-btn
       outline
       color="accent"
       label="Release tokens"
       class="hover-accent self-end q-mt-md"
-      v-if="
-        hasAllocations &&
-          Date.now() > pool.public_end + poolclosedelay &&
-          pool.pool_status === 'completed'
-      "
+      v-if="canRelease"
       @click="tryClosePool"
     />
     <q-btn
@@ -45,12 +56,10 @@
       color="accent"
       label="Claim"
       class="hover-accent self-end q-mt-md"
-      v-if="
-        hasAllocations && ['filled', 'cancelled'].includes(pool.pool_status)
-      "
+      v-if="canClaim"
       @click="tryClaimTokens"
     />
-    <p class="q-pt-md text-grey-6" v-if="!hasAllocations">
+    <p class="q-pt-md text-grey-6" v-if="!hasAllocation">
       No allocation to show.
     </p>
     <q-inner-loading :showing="loadingData">
@@ -73,47 +82,66 @@ export default {
     return {
       loadingData: true,
       allocationData: {},
-      poolclosedelay: 86400000 //24 hours to miliseconds
-      // poolclosedelay: 0 //24 hours to miliseconds
+      poolCloseDelay: 86400000 //24 hours to miliseconds
+      // poolCloseDelay: 0 //24 hours to miliseconds
     };
   },
 
   computed: {
     ...mapGetters("account", ["isAuthenticated", "accountName"]),
-    hasAllocations() {
+    hasAllocation() {
       return Object.keys(this.allocationData).length > 0;
+    },
+    canRelease() {
+      return (
+        this.hasAllocation &&
+        Date.now() > this.pool.public_end + this.poolCloseDelay &&
+        this.pool.pool_status === "completed"
+      );
+    },
+    canClaim() {
+      return (
+        this.hasAllocation &&
+        ["filled", "cancelled"].includes(this.pool.pool_status)
+      );
     },
     allocation() {
       return this.$chainToQty(this.allocationData.allocation);
     },
-    claimable() {
-      if (this.hasAllocations) {
+    available() {
+      if (this.hasAllocation) {
+        let allocation = this.allocation;
         if (this.pool.token_lockup) {
+          // TODO Check logic of this condition
+          let now = Date.now();
           let start = new Date(
             this.allocationData.lockup_start + "Z"
           ).valueOf();
           let percent = this.allocationData.lockup_percent / 10000;
-          let period = this.allocationData.lockup_period;
+          let period = this.allocationData.lockup_period * 1000 * 60 * 60 * 24;
+          let end = start + period;
           console.log(
-            `Start: ${start}, Percent: ${percent}, Period: ${period}`
+            `Start: ${start}, Now: ${now}, End: ${end}, Percent: ${percent}, Period: ${period}`
           );
-          console.log(`Date.now() - start: ${Date.now() - start}`);
-          console.log(`Alloc: ${this.allocation}`);
-          return (
-            this.allocation *
-            (1 - (percent * (Date.now() - start)) / period - 1)
-          );
-        } else return this.allocation;
+          if (now < start) {
+            return allocation * (1 - percent) - this.distributed;
+          } else if (start <= now && now <= end) {
+            const f = (now - start) / period;
+            return allocation * (1 + percent * (f - 1)) - this.distributed;
+          } else {
+            return allocation - this.distributed;
+          }
+        } else return allocation;
       } else return 0;
     },
-    claimed() {
-      if (this.hasAllocations) {
+    distributed() {
+      if (this.hasAllocation) {
         return this.$chainToQty(this.allocationData.distributed);
       } else return 0;
     },
     vesting() {
-      if (this.hasAllocations) {
-        return this.allocation - this.claimable - this.claimed;
+      if (this.hasAllocation) {
+        return this.allocation - this.available - this.distributed;
       } else return 0;
     },
     tokenSymbol() {
