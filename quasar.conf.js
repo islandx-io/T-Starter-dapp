42648882ba67155efffa7634b9f1eb20ea126982
Ja-Q <jpbeukes27@gmail.com>
2021-09-10 14:17:53 +0200

    added web3 to boot files

--- quasar.conf.js
+++ quasar.conf.js
@@ -30,7 +30,8 @@ module.exports = function(/* ctx */) {
       "poolinfo",
       "ballotinfo",
       "helpers",
-      "moonpay"
+      "moonpay",
+      "web3"
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
