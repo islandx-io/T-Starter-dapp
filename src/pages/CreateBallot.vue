6a3f6188041bc711b786b1470e85ccf31c608a7e
Dewald928 <dewald123@rocketmail.com>
2021-11-16 16:33:34 +0200

    fix url regex

--- src/pages/CreateBallot.vue
+++ src/pages/CreateBallot.vue
@@ -981,14 +981,7 @@ export default {
     },
 
     validURL(str) {
-      var pattern = new RegExp(
-        "^(https?:\\/\\/)?" + // protocol
-        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-        "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-        "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-          "(\\#[-a-z\\d_]*)?$",
-        "i"
+      var pattern = new RegExp("((http|https)://)(www.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)"
       ); // fragment locator
       if (str.length === 0) {
         return;
